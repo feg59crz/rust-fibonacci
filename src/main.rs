25a403d5853d5364cf58ce5f39afc80a2c303498
Fernando Gomes <fernandogomescruz@gmail.com>
2023-03-19 17:19:15 -0300

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn fib(n: usize) -> usize {
+    if n <= 1 {
+        return n;
+    }
+    fib(n - 1) + fib(n - 2)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", fib(9));
 }
